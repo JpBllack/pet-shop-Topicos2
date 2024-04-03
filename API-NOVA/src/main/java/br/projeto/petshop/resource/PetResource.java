adcaf0167f2fbd9a830fd12a88c1c8c2a5c981ad
JpBllack <joaogomes@unitins.br>
2024-04-03 19:13:44 -0300

    insert pet funcionando

--- API-NOVA/src/main/java/br/projeto/petshop/resource/PetResource.java
+++ API-NOVA/src/main/java/br/projeto/petshop/resource/PetResource.java
@@ -5,7 +5,6 @@ import org.jboss.logging.Logger;
 import br.projeto.petshop.service.PetService;
 import br.projeto.petshop.validation.ValidationException;
 import jakarta.inject.Inject;
-import jakarta.transaction.Status;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
@@ -18,7 +17,6 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
 import br.projeto.petshop.application.Error;
 import br.projeto.petshop.dto.PetDTO;
 
@@ -37,17 +35,17 @@ public class PetResource {
     @Path("/create")
     public Response insert(PetDTO dto){
         try{
-            return Response.status(Status.CREATED).entity(petService.criarPet(dto.build);
+            return Response.status(Response.Status.CREATED).entity(petService.criarPet(dto)).build();
         } catch (ValidationException e){
             LOG.error("Erro ao criar consulta");
             e.printStackTrace();
             Error error = new Error("400", e.getMessage());
-            return Response.status(Status.BAD_REQUEST).entity(error).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity(error).build();
         } catch (NotFoundException e){
             LOG.error("Veterinario ou Pet não encontrados");
             e.printStackTrace();
             Error error = new Error("404", e.getMessage());
-            return Response.status(Status.NOT_FOUND).entity(error).build();
+            return Response.status(Response.Status.NOT_FOUND).entity(error).build();
         }
     }
     
