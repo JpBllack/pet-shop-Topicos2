9de47b9200a1228666aefa51a0e87e545155b35a
alissonlima086 <alissonlima086@gmail.com>
2024-04-04 20:27:42 -0300

    pet

--- angular-pet-shop/src/app/pet/components/pet-list/pet-list.component.ts
+++ angular-pet-shop/src/app/pet/components/pet-list/pet-list.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { pet } from "../../../models/pet";
 import { PetService } from "../../../services/pet.service";
+import { NgFor } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterModule } from "@angular/router";
 
 @Component({
     selector: 'app-racao-list',
     standalone: true,
+    imports: [NgFor, MatTableModule, MatToolbarModule, MatIconModule
+        , MatButtonModule, RouterModule],
     templateUrl: './pet-list.component.html',
     styleUrl: './pet-list.component.css'
 })
