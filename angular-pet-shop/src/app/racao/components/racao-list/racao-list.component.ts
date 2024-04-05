d74cd5e017a9fa3f929edb09d49429670a034dc1
alissonlima086 <alissonlima086@gmail.com>
2024-04-05 07:37:47 -0300

    atualizado

--- angular-pet-shop/src/app/racao/components/racao-list/racao-list.component.ts
+++ angular-pet-shop/src/app/racao/components/racao-list/racao-list.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from "@angular/core";
-import { pet } from "../../../models/pet";
-import { PetService } from "../../../services/pet.service";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatTableModule } from "@angular/material/table";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { RouterModule } from "@angular/router";
+
+import { Racao } from '../../../models/racao.model';
+import { RacaoService } from '../../../services/racao.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-pet-list',
-    standalone: true,
-    imports: [MatTableModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
-    templateUrl: './racao-list.component.html',
-    styleUrls: ['./racao-list.component.css']
+  selector: 'app-racao-list',
+  standalone : true,
+  templateUrl: './racao-list.component.html',
+  styleUrls: ['./racao-list.component.css'],
+  imports: [
+    MatTableModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule
+  ]
 })
+export class RacaoListComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'sabor', 'animal', 'peso', 'idade', 'acao'];
+  racoes: Racao[] = [];
 
-export class PetListComponent implements OnInit {
-    displayedColumns: string[] = ['id', 'nome', 'usuario', 'tipoAnimal', 'anoNascimento'];
-    pets: pet[] = [];
+  constructor(private racaoService: RacaoService) {}
 
-    constructor(private petService: PetService) {}
-
-    ngOnInit(): void {
-        this.petService.findAll().subscribe((data: pet[]) => {
-            this.pets = data;
-        })
-    }
+  ngOnInit(): void {
+    this.racaoService.getAllRacoes().subscribe((data: Racao[]) => {
+      this.racoes = data;
+    });
+  }
 }
