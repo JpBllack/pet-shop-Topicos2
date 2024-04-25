104a8d5ada1e3e557f7e803ed00c833e5c2f09fe
alissonlima086 <alissonlima086@gmail.com>
2024-04-25 12:12:44 -0300

    consultaFuncional

--- angular-pet-shop/src/app/services/consulta.service.ts
+++ angular-pet-shop/src/app/services/consulta.service.ts
@@ -14,36 +14,21 @@ export class ConsultaService {
 
   constructor(private httpClient: HttpClient) {}
 
-  salvarConsulta(consulta: Consulta): Observable<Consulta> {
-    return this.httpClient.post<Consulta>(this.baseUrl, consulta);
-  }
-
-  buscarConsultaPorId(id: number): Observable<Consulta> {
-    return this.httpClient.get<Consulta>(`${this.baseUrl}/${id}`);
-  }
-
-  removerConsulta(id: number): Observable<any> {
-    const url = `${this.baseUrl}/delete/${id}`; // ajuste para o caminho de exclusão correto
-    return this.httpClient.delete<any>(url).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 404) {
-          return throwError('Consulta não encontrada para exclusão');
-        }
-        return throwError('Erro ao excluir consulta');
-      })
-    );
-  }
-
   listarTodasConsultas(): Observable<Consulta[]> {
     return this.httpClient.get<Consulta[]>(this.baseUrl);
   }
 
-  atualizarConsulta(id: number, consulta: Consulta): Observable<Consulta> {
-    return this.httpClient.put<Consulta>(`${this.baseUrl}/${id}`, consulta);
+  buscarConsultaPorId(id: number): Observable<Consulta> {
+    return this.httpClient.get<Consulta>(`${this.baseUrl}/search/${id}`);
   }
 
-  criarConsulta(consultaData: any): Observable<any> {
-    return this.httpClient.post<any>(this.baseUrl, consultaData);
+  atualizarConsulta(id: number, consulta: Consulta): Observable<Consulta> {
+    return this.httpClient.put<Consulta>(`${this.baseUrl}/atualizar/${id}`, consulta);
+  }
+
+  criarConsulta(consulta: any): Observable<any> {
+    const url = `${this.baseUrl}/create`;
+    return this.httpClient.post<Consulta>(url, consulta);
   }
 
   excluirConsulta(id: number): Observable<any> {
