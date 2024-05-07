3c7fe109aedbba88fd1104555cf305676d7da136
alissonlima086 <alissonlima086@gmail.com>
2024-05-07 10:35:30 -0300

    MALDITO TOKEN QUEBROU O LOGIN

--- angular-pet-shop/src/app/pages/login/login.component.ts
+++ angular-pet-shop/src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators, FormGroup } from "@angular/forms";
 import { ActivatedRoute, Router, RouterModule } from "@angular/router";
 
 import { Login } from "../../models/login";
@@ -13,22 +13,35 @@ import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIcon } from "@angular/material/icon";
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { JwtModule, JwtHelperService } from '@auth0/angular-jwt';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { LocalStorageService } from "../../services/localStorage.service";
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [FormsModule, HttpClientModule, NgIf, ReactiveFormsModule, MatFormFieldModule,
-        MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, RouterModule, MatSelectModule, CommonModule, MatIcon],
+    imports: [CommonModule, FormsModule, HttpClientModule, NgIf, ReactiveFormsModule, MatFormFieldModule,
+        MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, RouterModule, MatSelectModule, CommonModule, MatIcon, JwtHelperService],
     templateUrl: './login.component.html',
-    styleUrl: './login.component.css'
+    styleUrl: './login.component.css',
+    providers: [JwtHelperService]
 })
 export class LoginComponent implements OnInit{
 
     senhaVisivel: boolean = false;
-    formGroup: FormGroup;
+    loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router, private loginService: LoginService, private activatedRoute: ActivatedRoute){
-        this.formGroup = formBuilder.group({
+    constructor(private formBuilder: FormBuilder, 
+      private http: HttpClient, 
+      private router: Router, 
+      private loginService: LoginService, 
+      private activatedRoute: ActivatedRoute, 
+      private localStorageService: LocalStorageService, 
+      private snackBar: MatSnackBar){
+        this.loginForm = formBuilder.group({
             email: ['', Validators.required],
             senha: ['', Validators.required]
         })
@@ -37,29 +50,51 @@ export class LoginComponent implements OnInit{
     ngOnInit(): void {
         const login: Login = this.activatedRoute.snapshot.data['login'];
         if(login){
-            this.formGroup.patchValue(login)
+            this.loginForm.patchValue(login)
         }
     }
 
     login() {
-        if (this.formGroup.valid) {
-          const login = this.formGroup.value;
+        if (this.loginForm.valid) {
+          const login = this.loginForm.value;
           this.loginService.login(login).subscribe(
-            (response) => {
-              alert("Login bem sucedido");
-              // Redireciona para a página de dados do usuário logado
-              this.router.navigate(['/usuarios/meus-dados']);
-            },
-            (error) => {
-              alert("Erro ao logar");
-              // Tratar o erro de login, exibir mensagem de erro
+            {
+              next: (resp) => {
+
+                if (resp.body.perfil == "ADMIN") {
+      
+                  this.router.navigateByUrl('/admin');
+                }
+      
+                else {
+                  //this.showSnackbarTopPosition(this.authService.getToken(), 'Fechar', 2000);
+                  // redirecionar para a página principal
+                  this.router.navigateByUrl('/user/compras/produtos');
+                }
+              },
+              error: (err) => {
+                console.log(err);
+                this.showSnackbarTopPosition("Usuário ou senha Inválidos ⚠️", 'Fechar', 2000);
+              }
             }
           );
+        } else {
+          this.showSnackbarTopPosition("Dados inválidos ⛔", 'Fechar', 2000);
         }
       }
+    
+      
 
     toggleSenhaVisivel() {
         this.senhaVisivel = !this.senhaVisivel;
+    }
+
+    showSnackbarTopPosition(content:any, action:any, duration:any) {
+      this.snackBar.open(content, action, {
+        duration: 2000,
+        verticalPosition: "top", // Allowed values are  'top' | 'bottom'
+        horizontalPosition: "center" // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+      });
     }
     
 }
