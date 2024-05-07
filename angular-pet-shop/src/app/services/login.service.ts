944afd318737672fde96d4f8493ab8c49dd84b6a
alissonlima086 <alissonlima086@gmail.com>
2024-05-07 13:52:20 -0300

    Retirando pré implementação do token

--- angular-pet-shop/src/app/services/login.service.ts
+++ angular-pet-shop/src/app/services/login.service.ts
@@ -1,90 +1,45 @@
 import { HttpClient } from "@angular/common/http";
-import { Inject, Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 import { Login } from "../models/login";
-import { Usuario } from "../models/Usuario";
-import { LocalStorageService } from "./localStorage.service";
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { UsuarioService } from "./usuario.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class LoginService {
     private apiUrl = 'http://localhost:8080/auth';
-    private tokenKey = 'jwt_token';
-    private usuarioLogadoKey = 'usuario_logado';
-    private usuarioLogadoSubject = new BehaviorSubject<Usuario | null>(null);
+    private jwtTokenKey = 'jwt_token';
 
-    constructor(private http: HttpClient,
-        @Inject(LocalStorageService) private localStorageService: LocalStorageService,
-        private jwtHelper: JwtHelperService,
-        private usuarioService: UsuarioService
-    ) { }
+    constructor(private http: HttpClient) { }
 
     login(login: Login): Observable<any> {
         const url = `${this.apiUrl}/login`;
-        return this.http.post<any>(url, login, { observe: 'response' }).pipe(
-            tap((res: any) => {
-                const authToken = res.headers.get('Authorization') ?? '';
-                if (authToken) {
-                  this.setToken(authToken);
-                  const usuarioLogado = res.body;
-                  console.log(usuarioLogado);
-                  if (usuarioLogado) {
-                    this.setUsuarioLogado(usuarioLogado);
-                    this.usuarioLogadoSubject.next(usuarioLogado);
-                  }
+        return this.http.post<any>(url, login).pipe(
+            tap(response => {
+                if (response && response.token) {
+                    // Armazenar o token JWT no armazenamento local
+                    localStorage.setItem(this.jwtTokenKey, response.token);
                 }
-              })
+            })
         );
     }
 
-    private initUsuarioLogado() {
-    const usuario = localStorage.getItem(this.usuarioLogadoKey);
-    if (usuario) {
-      const usuarioLogado = JSON.parse(usuario);
-
-      this.setUsuarioLogado(usuarioLogado);
-      this.usuarioLogadoSubject.next(usuarioLogado);
+    getToken(): string | null {
+        // Recuperar o token JWT do armazenamento local
+        return localStorage.getItem(this.jwtTokenKey);
     }
-  }
 
-    setUsuarioLogado(usuario: Usuario): void {
+    isLoggedIn(): boolean {
+        // Lógica para verificar se o usuário está autenticado
+        // Por exemplo, verificar se há um token JWT armazenado no localStorage
+        const token = localStorage.getItem('jwt_token');
+        return token !== null; // Retorna true se houver um token, caso contrário, retorna false
+      }
 
-        this.localStorageService.setItem(this.usuarioLogadoKey, usuario);
-      }
-    
-      updateUsuarioLogado(usuario: any): void {
-    
-        this.localStorageService.setItem(this.usuarioLogadoKey, usuario);
-        this.usuarioLogadoSubject.next(usuario);
-      }
-    
-      setToken(token: string): void {
-        this.localStorageService.setItem(this.tokenKey, token);
-      }
-    
-      getUsuarioLogado() {
-        return this.usuarioLogadoSubject.asObservable();
-      }
-    
-      getToken(): string | null {
-        return this.localStorageService.getItem(this.tokenKey);
-      }
-    
-      removeToken(): void {
-        this.localStorageService.removeItem(this.tokenKey);
-      }
-    
-      removeUsuarioLogado(): void {
-        this.localStorageService.removeItem(this.usuarioLogadoKey);
-        this.usuarioLogadoSubject.next(null);
-      }
-    
-      isTokenExpired(): boolean {
-        const token = this.getToken();
-        return !token || this.jwtHelper.isTokenExpired(token);
+      logout(): void {
+        // Lógica para fazer logout
+        // Por exemplo, remover o token JWT do localStorage
+        localStorage.removeItem('jwt_token');
       }
 }
